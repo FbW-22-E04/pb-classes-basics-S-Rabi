b450a54edea91e3f94701ea9c135631732945243
S Rabi <rabi.sanaye@yahoo.com>
2022-08-25 07:43:22 +0200

    first

---
+++ index.js
@@ -0,0 +1,28 @@
+const now = new Date();
+const test = new Date();
+
+console.log(now);
+
+// 1: now is an instance of Date..
+
+// 2: The type on now is "object".
+
+// 3: A built-in Class
+
+// 4: The type of Date is function here.
+
+// 5: 2022-08-24T13:22:18.873Z
+
+// 6: An array like this. [Function: Date]
+
+// 7: 2022-08-24T13:22:18.873Z
+
+// 8: I don't know
+
+// 9: We receive a nice false. Because they are different instances of the Date class with different internal data
+
+// 10: We receive again nice false.
+
+// 11: We receive again a nice false.
+
+// 12: We receive a nice false. we are creating two separate instances of Date, both with their own separate internal data and identity
